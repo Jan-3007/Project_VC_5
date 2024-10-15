26067f12c21d47010351418ba5feab126300667a
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2024-10-15 09:33:14 +0200

    changed FreeRTOS from SMP port to non SMP port

--- Firmware/VC5/main.cpp
+++ Firmware/VC5/main.cpp
@@ -2,19 +2,31 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "RP2040.h"
 
-
+// check consistency with __NVIC_PRIO_BITS given in FreeRTOSConfig.h
 #if (__NVIC_PRIO_BITS != 2)
-    #error "unexpected value for __NVIC_PRIO_BITS "
+    #error unexpected value for __NVIC_PRIO_BITS
 #endif
 
 
 /* FreeRTOS includes. */
-#include <FreeRTOS.h>
-#include <task.h>
-#include <queue.h>
-#include <timers.h>
-#include <semphr.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+#include "semphr.h"
+
+
+
+
+
+#if LIB_PICO_MULTICORE
+    #error dont want to support multicore
+#endif
+
+
+
 
 
 
