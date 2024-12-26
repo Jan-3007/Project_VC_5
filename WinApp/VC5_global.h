604d3a9c3cc24f988acba05b821744761c5e2bce
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2024-12-26 14:31:20 +0100

    initial setup of windows command line program

---
+++ WinApp/VC5_global.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// C++
+
+
+
+// Windows
+// exclude GUI stuff to speed up compilation
+#define WIN32_LEAN_AND_MEAN	1
+#include <Windows.h>
+#include <SetupAPI.h>
+#pragma comment(lib, "setupapi.lib")
+
+
+
+// WinUSB
+#include <winusb.h>
+#pragma comment(lib, "winusb.lib")
+
+
+
+// VC5
+#include "WinUtils.h"
+#include "usb_protocol.h"
---
+++ WinApp/WinUtils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+using WinError = DWORD;
+
